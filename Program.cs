9868649b76c66a524acc8c912d4d674f7978ec99
Vulnorn <mixadrbk31@gmail.com>
2024-11-26 13:15:39 +0300

    Упрощение

--- Program.cs
+++ Program.cs
@@ -10,38 +10,29 @@ namespace Queue
         {
             Queue<int> shoppingQueue = new Queue<int>();
             Random random = new Random();
-            bool hasQueue = true;
             int firstValueForTest = 0;
             int secondValueForTest = 10;
             int numberBuyers = random.Next(firstValueForTest,secondValueForTest);
             int purchaseAmount;
             int accountTotal =0;
 
-            while (numberBuyers !=0)
+            for (int i = numberBuyers; i > 0; i--)
             {
                 purchaseAmount = random.Next(firstValueForTest, secondValueForTest);
                 shoppingQueue.Enqueue(purchaseAmount);
-                numberBuyers--;
             }
 
-            while (hasQueue)
+            while (shoppingQueue.Count != 0)
             {
                 Console.WriteLine($"Счет текущего клиента: {shoppingQueue.First()}. \nСчет Магазина: {accountTotal}");
                 accountTotal += shoppingQueue.Dequeue();
                 Console.WriteLine($"Клиент совершил покупку. Счет магазина: {accountTotal}");
                 Console.ReadKey();
-
-                if (shoppingQueue.Count!=0)
-                {
-                    Console.Clear();
-                }
-                else
-                {
-                    hasQueue = false;
-                    Console.WriteLine($"Очередь закончилась, вы обслужили всех клиентов");
-                    Console.ReadKey();
-                }
+                Console.Clear();
             }
+
+            Console.WriteLine($"Очередь закончилась, вы обслужили всех клиентов");
+            Console.ReadKey();
         }
     }
 }
